a32bc0a9bc50ffd184343f8fac70cc171f0d60e1
redcliphaloe <redcliphaloe@gmail.com>
2022-12-03 21:34:15 +0300

    feat: implement fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import fsp from 'fs/promises';
+import {getCurrentDirNameByURL} from '../utils/files.js';
+
 const create = async () => {
-    // Write your code here 
+  const dirName = getCurrentDirNameByURL(import.meta.url);
+  const subDirName = 'files';
+  const fileName = 'fresh.txt';
+  const filePath = path.join(dirName, subDirName, fileName);
+  const fileText = 'I am fresh and young';
+  const errorText = 'FS operation failed';
+
+  try {
+    await fsp.writeFile(filePath, fileText, {flag: 'wx'});
+  } catch (error) {
+    throw new Error(errorText);
+  }
 };
 
 await create();
