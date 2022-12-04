9ec43b21daccf238146df0445592082cd7240de5
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 19:33:12 +0300

    feat: implement streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import stream from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const errorText = 'FS operation failed';
+  const ts = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, `${chunk.toString().split('').reverse().join('')}\n`);
+    },
+  });
+
+  stream.pipeline(process.stdin, ts, process.stdout, () => {throw new Error(errorText)});
 };
 
 await transform();
