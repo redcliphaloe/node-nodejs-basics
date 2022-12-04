fa9ff33dec738ef34ba6c74950e70bc128dbfd9a
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 16:23:00 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import fsp from 'fs/promises';
+import {getCurrDirPathByURL} from '../utils/files.js';
+
 const list = async () => {
-    // Write your code here 
+  const currDirPath = getCurrDirPathByURL(import.meta.url);
+  const srcDirPath = path.join(currDirPath, 'files');
+  const errorText = 'FS operation failed';
+
+  try {
+    const list = await fsp.readdir(srcDirPath, {withFileTypes: true});
+    list.forEach(item => {
+      if (item.isFile()) {
+        console.log(item.name);
+      }
+    });
+  } catch (error) {
+    throw new Error(errorText);
+  }
 };
 
 await list();
