76b689bff64d905a80c606d4eebcc1684e41d02a
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 16:30:17 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fsp from 'fs/promises';
+import {getCurrDirPathByURL} from '../utils/files.js';
+
 const read = async () => {
-    // Write your code here 
+  const currDirPath = getCurrDirPathByURL(import.meta.url);
+  const srcFilePath = path.join(currDirPath, 'files', 'fileToRead.txt');
+  const errorText = 'FS operation failed';
+
+  try {
+    const content = await fsp.readFile(srcFilePath, {encoding: 'utf-8'});
+    console.log(content);
+  } catch (error) {
+    throw new Error(errorText);
+  }
 };
 
 await read();
