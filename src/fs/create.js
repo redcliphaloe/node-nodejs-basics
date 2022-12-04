135a50f4991ae7bfb90329599add48e38cc23b7a
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 15:52:57 +0300

    refactor: change fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,14 +4,12 @@ import {getCurrDirPathByURL} from '../utils/files.js';
 
 const create = async () => {
   const currDirPath = getCurrDirPathByURL(import.meta.url);
-  const subDirName = 'files';
-  const fileName = 'fresh.txt';
-  const filePath = path.join(currDirPath, subDirName, fileName);
-  const fileText = 'I am fresh and young';
+  const newFilePath = path.join(currDirPath, 'files', 'fresh.txt');
+  const newFileText = 'I am fresh and young';
   const errorText = 'FS operation failed';
 
   try {
-    await fsp.writeFile(filePath, fileText, {flag: 'wx'});
+    await fsp.writeFile(newFilePath, newFileText, {flag: 'wx'});
   } catch (error) {
     throw new Error(errorText);
   }
