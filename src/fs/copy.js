2ae621a15139858fe9e0509e68f3a67dab5c2709
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 14:17:51 +0300

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fsp from 'fs/promises';
+import {getCurrDirPathByURL} from '../utils/files.js';
+
 const copy = async () => {
-    // Write your code here 
+  const currDirPath = getCurrDirPathByURL(import.meta.url);
+  const srcDirPath = path.join(currDirPath, 'files');
+  const dstDirPath = path.join(currDirPath, 'files_copy');
+  const errorText = 'FS operation failed';
+
+  try {
+    await fsp.cp(srcDirPath, dstDirPath, {errorOnExist: true, force: false, recursive: true});
+  } catch (error) {
+    throw new Error(errorText);
+  }
 };
 
 copy();
