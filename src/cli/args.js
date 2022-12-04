aaced7bf45f9a924cba9b1ec4955831621767f48
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 17:18:59 +0300

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  let tempString = '';
+
+  process.argv.forEach((v, i) => {
+    if (i >= 2) {
+      if (v.indexOf('--') === 0) {
+        tempString += `${v.slice(2)} is `;
+      } else {
+        tempString += `${v}, `;
+      }
+    }
+  });
+  console.log(tempString.slice(0, -2));
 };
 
 parseArgs();
