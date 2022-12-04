ccddf64d9df8d19917fd2bac9f00e4fb9a518586
redcliphaloe <redcliphaloe@gmail.com>
2022-12-04 15:57:35 +0300

    feat: implement fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import fsp from 'fs/promises';
+import {getCurrDirPathByURL} from '../utils/files.js';
+
 const remove = async () => {
-    // Write your code here 
+  const currDirPath = getCurrDirPathByURL(import.meta.url);
+  const targetFilePath = path.join(currDirPath, 'files', 'fileToRemove.txt');
+  const errorText = 'FS operation failed';
+  
+  try {
+    await fsp.rm(targetFilePath);
+  } catch (error) {
+    throw new Error(errorText);
+  }
 };
 
 await remove();
