fc592710c6e08b9c25fae7086817f244c98217af
redcliphaloe <redcliphaloe@gmail.com>
2022-12-06 01:22:01 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import path from 'path';
+import cp from 'child_process';
+import {getCurrDirPathByURL} from '../utils/files.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const currDirPath = getCurrDirPathByURL(import.meta.url);
+  const childProcFilePath = path.join(currDirPath, 'files', 'script.js');
+  const childProc = cp.fork(childProcFilePath, args, {stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+
+  childProc.stdout.on('data', chunk => process.stdout.write(`Child process answer: ${chunk.toString()}`));
+  process.stdin.on('data', chunk => childProc.stdin.write(chunk.toString()));
 };
 
-spawnChildProcess();
+spawnChildProcess([true, 2, 'three']);
